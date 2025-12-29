f4db7401e79986327804ff348af09d1e6dc4aa66
h3xler <101924807+h3xler@users.noreply.github.com>
2025-12-29 17:12:35 +0300

    Update page.tsx

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -5,13 +5,11 @@ import { getAppConfig } from '@/lib/utils';
 export default async function Page(props: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
-  // URL'deki ?room=... parametresini yakalıyoruz
   const searchParams = await props.searchParams;
   const roomName = searchParams.room as string | undefined;
 
   const hdrs = await headers();
   const appConfig = await getAppConfig(hdrs);
 
-  // Yakaladığımız roomName'i App bileşenine prop olarak gönderiyoruz
   return <App appConfig={appConfig} roomName={roomName} />;
 }
